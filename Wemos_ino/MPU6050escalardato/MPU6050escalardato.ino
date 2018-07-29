9c86b5e9462394d5b999d07e9416af45571daf0f
David Castillo Alvarado <dcastilloa@uni.pe>
2018-07-29 18:55:00 -0500

    Add files via upload

---
+++ Wemos_ino/MPU6050escalardato/MPU6050escalardato.ino
@@ -0,0 +1,64 @@
+// Librerias I2C para controlar el mpu6050
+// la libreria MPU6050.h necesita I2Cdev.h, I2Cdev.h necesita Wire.h
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+
+// La dirección del MPU6050 puede ser 0x68 o 0x69, dependiendo 
+// del estado de AD0. Si no se especifica, 0x68 estará implicito
+MPU6050 sensor;
+
+// Valores RAW (sin procesar) del acelerometro y giroscopio en los ejes x,y,z
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+//Valor de los offsets
+int ax_o,ay_o,az_o;
+int gx_o,gy_o,gz_o;
+
+void setup() {
+  Serial.begin(57600);    //Iniciando puerto serial
+  Wire.begin();           //Iniciando I2C  
+  sensor.initialize();    //Iniciando el sensor
+
+  if (sensor.testConnection()) Serial.println("Sensor iniciado correctamente");
+  else Serial.println("Error al iniciar el sensor");
+}
+
+void loop() {
+  // Leer las aceleraciones y velocidades angulares
+  sensor.getAcceleration(&ax, &ay, &az);
+  sensor.getRotation(&gx, &gy, &gz);
+  float ax_m_s2 = ax * (9.81/16384.0);
+  float ay_m_s2 = ay * (9.81/16384.0);
+  float az_m_s2 = az * (9.81/16384.0);
+  float gx_deg_s = gx * (250.0/32768.0);
+  float gy_deg_s = gy * (250.0/32768.0);
+  float gz_deg_s = gz * (250.0/32768.0);
+  //Mostrar las lecturas separadas por un [tab]
+  Serial.print("a[x y z](m/s2) g[x y z](deg/s):\t");
+  Serial.print(ax_m_s2); Serial.print("\t");
+  Serial.print(ay_m_s2); Serial.print("\t");
+  Serial.print(az_m_s2); Serial.print("\t");
+  Serial.print(gx_deg_s); Serial.print("\t");
+  Serial.print(gy_deg_s); Serial.print("\t");
+  Serial.println(gz_deg_s);
+
+  delay(100);
+
+    ax_o=sensor.getXAccelOffset();
+  ay_o=sensor.getYAccelOffset();
+  az_o=sensor.getZAccelOffset();
+  gx_o=sensor.getXGyroOffset();
+  gy_o=sensor.getYGyroOffset();
+  gz_o=sensor.getZGyroOffset();
+  
+  Serial.println("Offsets:");
+  Serial.print(ax_o); Serial.print("\t"); 
+  Serial.print(ay_o); Serial.print("\t"); 
+  Serial.print(az_o); Serial.print("\t"); 
+  Serial.print(gx_o); Serial.print("\t"); 
+  Serial.print(gy_o); Serial.print("\t");
+  Serial.print(gz_o); Serial.print("\t");
+}
+ 
